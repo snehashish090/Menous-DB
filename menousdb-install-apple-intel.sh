5ebee00ed72e3f62137d6cc6252cb418125dec1a
Snehashish Laskar <snehashishlaskar@Snehashishs-MacBook-Air.local>
2024-05-26 21:54:37 +0530

    changes

---
+++ menousdb-install-apple-intel.sh
@@ -0,0 +1,5 @@
+curl -O https://github.com/MenousTech/Menous-DB/releases/download/1.0.3/menousdb-intel.tar.gz
+sudo mv menousdb-intel.tar.gz /usr/local/bin/menousdb-intel.tar.gz
+cd /usr/local/bin/
+tar -xvf menousdb-intel.tar.gz
+sudo rm menousdb-intel.tar.gz
---
+++ menousdb-install-apple-sillicon.sh
@@ -0,0 +1,4 @@
+curl -L -o menousdb-apple-sillicon.tar.gz "https://github.com/MenousTech/Menous-DB/releases/download/1.0.3/menousdb-apple-sillicon.tar.gz"
+sudo tar -xvf menousdb-apple-sillicon.tar.gz
+sudo rm menousdb-apple-sillicon.tar.gz
+sudo mv menousdb /usr/local/bin/
